dd8ece605e8a5156fdc9674092cad5d50f73c167
Jack Langston <jack.langston99@gmail.com>
2019-07-12 16:55:37 -0700

    Update before-plots

--- R/before-plots.R
+++ R/before-plots.R
@@ -9,7 +9,7 @@ ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, col = Species)) +
     geom_smooth(method = lm, se = FALSE) #geom_smooth only does full model
 
 model2 <- lm(Sepal.Width ~ Sepal.Length * Species, data = iris)
-ggPredict(model2)
+ggPredict(model2, se = T)
 
 plot_ly(iris, x = ~Sepal.Length, y = ~Sepal.Width, color = ~Species, type = 'scatter', showlegend = TRUE) %>%
     add_lines(x = ~Sepal.Length, y = fitted(model2))
@@ -81,7 +81,7 @@ polydata <- read_excel("C:/Users/jackl_000/Desktop/polydata.xlsx")
 ggplot(data = polydata, aes(x = x, y = y, col = cat)) +
     geom_point() +
     #geom_smooth(method = 'lm', se = FALSE, formula = y ~ x + I(x^2))
-    stat_smooth(method = 'lm', se = FALSE, formula = y ~ x + I(x^2))# also works
+    stat_smooth(method  = 'lm', se = FALSE, formula = y ~ x + I(x^2))# also works
 
 model4 <- lm(y ~ x + I(x^2) + cat, data = polydata)
 
@@ -92,7 +92,7 @@ ggplot(data = polydata, aes(x = x, y = y, col = cat)) +
         aes(col = 'A')) + 
     stat_function(fun=function(x) 
         model4$coefficients[1] + model4$coefficients[2]*x + model4$coefficients[3]*x^2 + model4$coefficients[4], 
-        aes(col = 'B'))
+        aes(col = 'B'), xlim = c(0,10))
 
 plot_ly(polydata, x = ~x, y = ~y, color = ~cat, type = 'scatter', showlegend = TRUE) %>%
     add_lines(x = ~x, y = predict(model4))
@@ -135,31 +135,56 @@ plot_ly(polydata, x = ~x, y = ~y, color = ~cat, type = 'scatter', showlegend = T
 
 
 #interactive
-model2 <- lm(Sepal.Width ~ Sepal.Length * Species, data = iris)
-ggPredict(model2)
+model1 <- lm(Sepal.Width ~ Sepal.Length + Species, data = iris)
+ggPredict(model1)
+ggPredict(model1, se = TRUE)
+ggPredict(model1, interactive = TRUE)
+ggPredict(model1, interactive = TRUE, se = TRUE)
 
-xname=names(model2$model)[2]
-yname=names(model2$model)[1]
+m <- model1$coefficients[2] #slope of model
+b <- model1$coefficients[1] #setosa intercept
 
-data <- model2$model
+x <- 'Sepal.Length'
+y <- 'Sepal.Width'
+
+data <- model1$model
 data$data_id=rownames(data)
-iris$tooltip=paste0(data$data_id,"\n",xname,"=",data[[xname]],"\n",yname,"=",data[[yname]])
-
-
-p <- ggplot(data = iris, aes(x = Sepal.Length,
-                             y = Sepal.Width, 
-                             col = Species, 
-                             fill = Species,
-                             tooltip = tooltip,
-                             group = Species))+
-    geom_point_interactive() +
-    geom_line_interactive()
+iris$tooltip=paste0(data$data_id,"\n",x," = ",data[[x]],"\n",y," = ",data[[y]])
 
 tooltip_css <- "background-color:white;font-style:italic;padding:10px;border-radius:10px 20px 10px 20px;"
 hover_css="r:4px;cursor:pointer;stroke:red;stroke-width:2px;"
 selected_css = "fill:#FF3333;stroke:black;"
 
-p <- ggiraph(code=print(p),
+setosaTT <- paste('Setosa \n', y, '=', sprintf("%.3f",m), '*', x, '+', sprintf("%.3f",b), sep = ' ')
+versicolorTT <- paste('Versicolor \n', y, '=', sprintf("%.3f",m), '*', x, '+', sprintf("%.3f",b+model1$coefficients[3]), sep = ' ')
+virginicaTT <- paste('Virginica \n', y, '=', sprintf("%.3f",m), '*', x, '+', sprintf("%.3f",b+model1$coefficients[4]), sep = ' ')
+
+p <- ggplot(data = iris, aes(x = Sepal.Length,
+                             y = Sepal.Width, 
+                             col = Species, 
+                             fill = Species,
+                             group = Species)) + 
+    geom_segment_interactive(aes(x = min(iris[iris$Species == 'setosa',]$Sepal.Length),
+                     xend = max(iris[iris$Species == 'setosa',]$Sepal.Length),
+                     y = min(iris[iris$Species == 'setosa',]$Sepal.Length)*m+b,
+                     yend = max(iris[iris$Species == 'setosa',]$Sepal.Length)*m+b,
+                     color = 'setosa',
+                     tooltip = setosaTT)) +
+    geom_segment_interactive(aes(x = min(iris[iris$Species == 'versicolor',]$Sepal.Length),
+                     xend = max(iris[iris$Species == 'versicolor',]$Sepal.Length),
+                     y = min(iris[iris$Species == 'versicolor',]$Sepal.Length)*m+b+model1$coefficients[3],
+                     yend = max(iris[iris$Species == 'versicolor',]$Sepal.Length)*m+b+model1$coefficients[3],
+                     color = 'versicolor',
+                     tooltip = versicolorTT)) +
+    geom_segment_interactive(aes(x = min(iris[iris$Species == 'virginica',]$Sepal.Length),
+                     xend = max(iris[iris$Species == 'virginica',]$Sepal.Length),
+                     y = min(iris[iris$Species == 'virginica',]$Sepal.Length)*m+b+model1$coefficients[4],
+                     yend = max(iris[iris$Species == 'virginica',]$Sepal.Length)*m+b+model1$coefficients[4], 
+                     color = 'virginica',
+                     tooltip = virginicaTT)) +
+    geom_point_interactive(aes(tooltip = tooltip, data_id = data$data_id))
+
+ggiraph(code=print(p),
              tooltip_extra_css=tooltip_css,
              tooltip_opacity=.75,
              zoom_max=10,
